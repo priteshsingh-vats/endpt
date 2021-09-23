2c965f8be52936ea29ac83446bd938d32c776bd4
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2021-09-23 11:44:35 +0530

    Update EmployeeApplication.java

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeApplication {
+public class EmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
